3ddea0d140c0ddbb22503caf9424e2ff1f6368fb
amber-king <amberking@pursuit.org>
2023-06-26 14:27:23 -0400

    create a linked w/ properties - head/no methods yet

--- solution.js
+++ solution.js
@@ -1,5 +1,20 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;s
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  
+  }
+  
+}
+
 module.exports = {
   Node,
   LinkedList,
