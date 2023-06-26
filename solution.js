f0ebedf1677dc6792e63c978215cf264e1fe3404
amber-king <amberking@pursuit.org>
2023-06-26 14:32:30 -0400

    checking if nodes are there to link

--- solution.js
+++ solution.js
@@ -3,16 +3,19 @@ const { nums, words } = require("./data/data.js");
 class Node {
   constructor(data) {
     this.data = data;
-    this.next = null;s
+    this.next = null;
   }
 }
 
+let oneNode = new Node(1);
+let twoNode = new Node(2);
+oneNode.next = twoNode;
+console.log(oneNode);
+
 class LinkedList {
-  constructor(){
+  constructor() {
     this.head = null;
-  
   }
-  
 }
 
 module.exports = {
