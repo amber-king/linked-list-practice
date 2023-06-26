ccb82d11521b768b8baeb6087213a8100bbfd5b8
amber-king <amberking@pursuit.org>
2023-06-26 14:37:04 -0400

    checking for functionality of linked list set up

--- solution.js
+++ solution.js
@@ -7,16 +7,22 @@ class Node {
   }
 }
 
+// nodes created to link them
 let oneNode = new Node(1);
 let twoNode = new Node(2);
 oneNode.next = twoNode;
-console.log(oneNode);
+// console.log(oneNode); // Node { data: 1, next: Node { data: 2, next: null } }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
+  constructor(head = null) {
+    this.head = head;
   }
 }
+let listOne = new LinkedList(oneNode)
+console.log(listOne)  // --v
+// LinkedList {
+//   head: Node { data: 1, next: Node { data: 2, next: null } }
+// }
 
 module.exports = {
   Node,
