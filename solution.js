e47fb5175fe0bc3dd9845b04251fab8bd958b964
amber-king <amberking@pursuit.org>
2023-06-26 16:02:05 -0400

    getKth methods, isEmpty method & clear method done

--- solution.js
+++ solution.js
@@ -35,10 +35,10 @@ class LinkedList {
   //   ✓ Count the length of the linked list (1 ms)
   size() {
     let count = 0;
-    let pointer = this.head;
-    while (pointer) {
+    let node = this.head;
+    while (node) {
       count++;
-      pointer = pointer.next;
+      node = node.next;
     }
     return count;
   }
@@ -73,14 +73,64 @@ class LinkedList {
     }
     return node;
   }
-  search(key) {
+
+  // ✓ Find an element by key
+  search(findKey) {
     let node = this.head;
-    while (node !== null && node.data !== key) {
+    while (node !== null && node.data !== findKey) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  // Source : https://www.interviewbit.com/problems/kth-smallest-element-in-the-array/#:~:text=Definition%20of%20kth%20smallest%20element,sorted%2C%20then%20Ak%20%2D%201
+  // What is the KTH element?
+  // Definition of kth smallest element:
+  // The kth smallest element is the minimum possible n such that there are at least k elements in the array <= n.
+  // In other words, if the array A was sorted, then Ak - 1.
+
+  // ✓ Retrieve kth element
+  getKth(kthElement) {
+    let node = this.head;
+    let count = 1;
+    while (count !== kthElement && node.next) {
+      count++;
       node.next;
     }
     return node;
   }
+
+  //  ✓ Retrieve kth from last element
+  getKthToLast(kthLastElement) {
+    let node = this.head;
+    let amtOfNodes = 0;
+    while (node) {
+      amtOfNodes++;
+      node = node.next;
+    }
+    let count = 1;
+    node = this.head;
+    while (count < amtOfNodes - kthLastElement) {
+      count++;
+      node = node.next;
+    }
+    return node;
+  }
+
+  //  ✓ Check if list is empty
+  isEmpty(){
+  return !this.head ;
+  }
+  
+  
+//  ✓ Clear the linked list
+  clear() {
+    this.head = null; 
+  }
+  // toArray() {}
+  // containsDuplicates() {}
 }
+
 // ? LinkedList funtionailty test --v
 // let listOne = new LinkedList(oneNode)
 // console.log(listOne)  // --v
